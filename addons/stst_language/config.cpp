2e3b38e00949dbcab25fd065f5e5660f1b683412
SkippieDippie <skippiedipie@gmail.com>
2022-04-09 23:36:23 +0200

    Started the translation process.
    Added translations for some devices.

---
+++ addons/stst_language/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches {
+	class STST_Language {
+		author = "Skippie";
+		name = "STALKER Stuff - Texts and translations";
+		url = "https://github.com/SkippieDippie/Arma-3-STALKER-Stuff";
+
+		requiredAddons[] = {};
+		requiredVersion = 0.1;
+
+		units[]={};
+		weapons[]={};
+	}
+};
